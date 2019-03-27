69e51868a5a8665299418c0805f9681b5e2d4894
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 16:29:12 -0400

    reates a PUT request handler for requests to the '/:id' endpoint that will update values on the specified student in the 'students' table

--- students/students-router.js
+++ students/students-router.js
@@ -55,4 +55,20 @@ router.post("/", (req, res) => {
         });
 });
 
+router.put("/:id", (req, res) => {
+    db("students")
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ message: "Specified student was not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
 module.exports = router;
