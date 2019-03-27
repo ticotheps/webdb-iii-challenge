5f1fdfba8e60550b83a247abe5c55ddb33258f0c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 17:03:18 -0400

    Runs the 'npx knex seed:run' command in the terminal to make insert seed data into the 'cohorts' table

--- seeds/001-students.js
+++ seeds/001-students.js
@@ -1,13 +1,27 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('students').insert([
+        { 
+          name: 'Tico Thepsourinthone',
+          cohort_id: 1
+        },
+        { 
+          name: 'Lowell Richardson',
+          cohort_id: 1
+        },
+        { 
+          name: 'Jamie Goodnight',
+          cohort_id: 1
+        },
+        { 
+          name: 'Cecil Tantay',
+          cohort_id: 3
+        }
       ]);
     });
 };
