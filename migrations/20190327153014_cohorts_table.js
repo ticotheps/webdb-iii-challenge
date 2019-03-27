889eea09f2c242a5a86a7f2396df772ba53beefe
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 15:53:42 -0400

    Refactors cohorts_table.js

--- migrations/20190327153014_cohorts_table.js
+++ migrations/20190327153014_cohorts_table.js
@@ -1,6 +1,6 @@
 // the new changes we need to make
 exports.up = function(knex) {
-  return knex.schema.createTable('cohorts', function(tbl) {
+    return knex.schema.createTable('cohorts', function(tbl) {
       // primary key, called 'id' and make it auto-increment
       tbl.increments();
 
@@ -8,10 +8,10 @@ exports.up = function(knex) {
         .string('name', 128)
         .notNullable()
         .unique();
-  });
+    });
 };
 
 // how to undo the changes made in the function above
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('cohorts');
+    return knex.schema.dropTableIfExists('cohorts');
 };
