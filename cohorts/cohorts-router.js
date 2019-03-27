9f921a34d63944a218e9c96870b9153fa8a83ec9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 16:10:51 -0400

    Creates a GET request handler to return a specified cohort by its id

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -13,12 +13,28 @@ const knexConfig = {
 const db = knex(knexConfig);
 
 router.get("/", (req, res) => {
+    // retrieves all records from the cohorts table
     db("cohorts")
         .then(cohorts => {
             res.status(200).json(cohorts);
         })
         .catch(error => {
             res.status(500).json(error);
+        });
+});
+
+router.get("/:id", (req, res) => {
+    const cohortId = req.params.id; // this is the same thing as the desctructured version: 'const { id } = req.params'; 
+
+    // retrieves a specified cohort by its id
+    db("cohorts")
+        .where({ id: cohortId})
+        .first() // this makes sure to ONLY return the FIRST matching element found
+        .then(cohort => {
+            res.status(200).json(cohort);
+        })
+        .catch(error => {
+            res.status(500).json(error)
         });
 });
 
