accad8cc9227908394a3eda3165956a4a1186f40
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 16:52:46 -0400

    Modifies the values in the 001-cohorts.js file to be specific to the 'cohorts' table, replaces the .delete() knex method with the .truncate() knex method to reset the primary key/clean the table, and adds values to be inserted into the 'cohorts' table as new records

--- seeds/001-cohorts.js
+++ seeds/001-cohorts.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cohorts')
+    .truncate() // resets the primary key, in addition to cleaning the table
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cohorts').insert([
+        { name: 'Web17'},
+        { name: 'CS5'},
+        { name: 'Web16'}
       ]);
     });
 };
