ed32298f9d553abeed327ccb2b05ab5283909696
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 15:51:46 -0400

    Fully defines the 'exports.up' and 'exports.down' statements in the '20190327154710_students_table.js' file

--- migrations/20190327154710_students_table.js
+++ migrations/20190327154710_students_table.js
@@ -1,8 +1,18 @@
-
-exports.up = function(knex, Promise) {
+// the new changes we need to make
+exports.up = function(knex) {
+    return knex.schema.createTable('students', function(tbl) {
+        // primary key, called 'id' and make it auto-increment
+        tbl.increments();
   
-};
-
-exports.down = function(knex, Promise) {
+        tbl
+          .string('name', 128)
+          .notNullable()
+          .unique();
+    });
+  };
   
-};
+  // how to undo the changes made in the function above
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('students');
+  };
+  
