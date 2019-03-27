40109ff7f3b890e08c5c76b722f4ddb67337d469
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 15:12:40 -0400

    Imports cohortsRouter into server.js file and tells server to use the route for any requests to '/api/cohorts' endpoint

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const helmet = require('helmet');
 
-// const cohortsRouter = require('../cohorts/cohorts-router.js');
+const cohortsRouter = require('../cohorts/cohorts-router.js');
 // const studentsRouter = require('../students/students-router.js');
 
 const server = express();
@@ -9,7 +9,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-// server.use('/api/cohorts', cohortsRouter);
+server.use('/api/cohorts', cohortsRouter);
 // server.use('api/students', studentsRouter);
 
 module.exports = server;
