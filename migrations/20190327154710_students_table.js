acf54f69bdde921fc7e994ecb16571fa5c2d6cc7
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 15:57:09 -0400

    Adds a foreign key to the students_table.js file called 'cohort_id'

--- migrations/20190327154710_students_table.js
+++ migrations/20190327154710_students_table.js
@@ -1,18 +1,26 @@
 // the new changes we need to make
 exports.up = function(knex) {
-    return knex.schema.createTable('students', function(tbl) {
-        // primary key, called 'id' and make it auto-increment
-        tbl.increments();
-  
-        tbl
-          .string('name', 128)
-          .notNullable()
-          .unique();
-    });
-  };
-  
-  // how to undo the changes made in the function above
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('students');
-  };
-  
+  return knex.schema.createTable("students", function(tbl) {
+    // primary key, called 'id' and make it auto-increment
+    tbl.increments();
+
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+
+    // adds a foreign key
+    tbl
+      .integer("cohort_id")
+      .unsigned()
+      .references("id")
+      .inTable("students")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+// how to undo the changes made in the function above
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("students");
+};
